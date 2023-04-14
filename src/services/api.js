3085ca0c05e1e770acc8c23c90ae142c21d96089
Leidejane da Rosa <leidejanedaRosa.81@gmail.com>
2023-04-14 11:14:22 -0300

    Arrumando deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: process.env.baseURL,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
