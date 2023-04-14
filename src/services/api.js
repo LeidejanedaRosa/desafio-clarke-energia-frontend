5447ce53dbb244822499cbba888cf41bbd4c9d0a
Leidejane da Rosa <leidejanedaRosa.81@gmail.com>
2023-04-14 11:22:26 -0300

    Arrumando o deloy

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: process.env.baseURL,
+  baseURL: process.env.apiURL,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
